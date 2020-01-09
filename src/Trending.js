e7b8f1e6bff04c05bacd4008135321bfc02795d2
Emily Fang <fangemily60@gmail.com>
2020-01-09 18:38:49 -0500

    Tredning functionality works but CSS needed

--- src/Trending.js
+++ src/Trending.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react'
-import axios from 'axios'
-
-class Trending extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            trendingSearch:"",
-            data: []
-        }
-    }
-
-    handleChange = (event) => {
-        this.setState({trendingSearch: event.target.value});   
-    }
-
-    handleClick = () => {
-        axios.get("http://api.giphy.com/v1/gifs/trending?api_key=" + "X0JVNouDUsrGBIINWA2b2ixSJgK45sKl")
-            .then(response => {
-                this.setState({
-                    data: response.data.data
-                })
-                console.log(response.data.data)
-            })
-=======
 /* eslint-disable */
 import React, { Component } from 'react'
 import axios from 'axios'
@@ -44,21 +18,20 @@ class Trending extends Component {
 
         if(this.state.display===false)
         {
-        axios.get("http://api.giphy.com/v1/gifs/trending?api_key=" + "X0JVNouDUsrGBIINWA2b2ixSJgK45sKl")
-            .then(response => {
-                this.setState({
-                    data: response.data.data,
-                    display: true
-                })
-                console.log(response.data.data);
-                
-            })
+            axios.get("http://api.giphy.com/v1/gifs/trending?api_key=" + "X0JVNouDUsrGBIINWA2b2ixSJgK45sKl")
+		.then(response => {
+                    this.setState({
+			data: response.data.data,
+			display: true
+                    })
+                    console.log(response.data.data);
+                    
+		})
         }
         else
         {
             this.setState({display: true});
         }
->>>>>>> trending
     }
 
     determineDisplay = () => {
@@ -71,38 +44,20 @@ class Trending extends Component {
         {
             return (
                 this.state.data.map((element) => {
-<<<<<<< HEAD
-                    return (
-			    <div class="gif">
+                    return (<div class="gif">
                             <img src={element.images.original.url} alt="animated"></img>
                             </div>
-                    );
-                })
-            )	    
-=======
-                    return (<div class="gif">
-                        <img src={element.images.original.url} alt="animated"></img>
-                        </div>
-                        )
+                           )
                 })
             )
-
->>>>>>> trending
         }
     }
     render() {
         let display = this.determineDisplay();
         return (
-
-<<<<<<< HEAD
 		<div>
-                <input id="search" onChange={this.handleChange}></input>
-                <button onClick={this.handleClick}>Submit</button>
-=======
-            <div>
                 <button onClick={this.handleClick}>Trending</button>
->>>>>>> trending
-                {display}
+		{display}
             </div>
         )
     }
