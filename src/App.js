f306cbba0866ee0b03f494b3b6249c8cc3db6f95
Ethan <esam5556@gmail.com>
2020-01-09 21:35:30 -0500

    Added the random button on GifCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import GifCard from './Gifcard'
 import Trending from './Trending'
+
 
 class App extends Component {
   render() {
     return (
         <div>
-        <Trending />
+        <Trending /> 
+        <GifCard />
         </div>
     );
   }
--- src/Gifcard.js
+++ src/Gifcard.js
@@ -1,68 +1,63 @@
-/* eslint-disable */
 import React, { Component } from 'react'
 import axios from 'axios'
 
-
-class Trending extends Component {
-    constructor(props) { 
+class GifCard extends Component {
+    constructor(props) {
         super(props)
-        this.state = {
+        this.state = 
+        {
             data: [],
             display: false
         }
     }
-
-
-
     handleClick = () => {
 
-        if(this.state.display===false)
-        {
-        axios.get("http://api.giphy.com/v1/gifs/trending?api_key=X0JVNouDUsrGBIINWA2b2ixSJgK45sKl")
-            .then(response => {
-                this.setState({
-                    data: response.data.data,
-                    display: true
+        if (this.state.display === false) {
+            axios.get("http://api.giphy.com/v1/gifs/random?api_key=X0JVNouDUsrGBIINWA2b2ixSJgK45sKl")
+                .then(response => {
+                    this.setState({
+                        data: response.data,
+                        display: true
+                    })
+                    console.log(response.data);
+
                 })
-                console.log(response.data.data);
-                
-            })
         }
-        else
-        {
-            this.setState({display: true});
+        else {
+            this.setState({ display: true });
         }
     }
+
 
     determineDisplay = () => {
         //If no data is returned, then return nothing
         if (this.state.data.length === 0) 
         {
             return null;
-        } 
-        else 
-        {
+        }
+        else {
             return (
                 this.state.data.map((element) => {
                     return (<div class="gif">
                         <img src={element.images.original.url} alt="animated"></img>
-                        </div>
-                        )
+                    </div>
+                    )
                 })
             )
 
         }
     }
+
     render() {
         let display = this.determineDisplay();
         return (
-
             <div>
-                <button onClick={this.handleClick}>Trending</button>
+                <button onClick={this.handleClick}>Random</button>
                 {display}
             </div>
         )
     }
 }
 
-export default Trending;
+
+export default GifCard;
